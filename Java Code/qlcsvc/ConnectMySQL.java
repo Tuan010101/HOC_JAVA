7e717886b036f7ef07e850f738eaaac79117fea4
Tuan010101 <90946207+Tuan010101@users.noreply.github.com>
2022-11-01 23:45:39 +0700

    up connect sql

---
+++ Java Code/qlcsvc/ConnectMySQL.java
@@ -0,0 +1,34 @@
+package qlcsvc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class ConnectMySQL {
+    private static String DB_URL = "jdbc:mysql://localhost:3306/demosqljava";
+    private static String USER_NAME = "root";
+    private static String PASSWORD = "";
+
+    public static void main(String args[]) {
+        try {
+            // kết nối tới file dữ liệu 'demosqljava'
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection(DB_URL, USER_NAME, PASSWORD);
+            System.out.println("Connect succesfully!");
+            // tạo statement để kết nối csds
+            Statement stmt = conn.createStatement();
+            // lấy dữ liệu từ table 'student'
+            ResultSet rs = stmt.executeQuery("select * from student");
+            // hiển thị dữ liệu
+            while (rs.next()) {
+                System.out.println(rs.getInt("id") + "  " + rs.getString("name")
+                        + "  " + rs.getDouble("score"));
+            }
+            // ngắt kết nối
+            conn.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
